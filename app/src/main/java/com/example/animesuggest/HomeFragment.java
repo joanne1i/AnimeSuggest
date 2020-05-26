ba688c6d48b3e3a0d0ef864f984e7a60b6789485
joanne1i <joanneli@Joannes-MacBook-Pro.local>
2020-05-26 15:13:07 -0700

    connected to database

--- app/src/main/java/com/example/animesuggest/HomeFragment.java
+++ app/src/main/java/com/example/animesuggest/HomeFragment.java
@@ -6,23 +6,65 @@ import android.view.LayoutInflater;
 import android.view.View;
 import android.view.ViewGroup;
 import android.widget.Button;
+import android.widget.TextView;
 
 import androidx.annotation.NonNull;
 import androidx.annotation.Nullable;
 import androidx.fragment.app.Fragment;
 
+import com.apollographql.apollo.ApolloCall;
+import com.apollographql.apollo.ApolloClient;
+import com.apollographql.apollo.api.Response;
+import com.apollographql.apollo.exception.ApolloException;
+import com.example.AnimeDataQuery;
+import com.orhanobut.logger.AndroidLogAdapter;
+import com.orhanobut.logger.Logger;
+
+import org.jetbrains.annotations.NotNull;
+import org.w3c.dom.Text;
+
+import okhttp3.OkHttpClient;
+
 public class HomeFragment extends Fragment {
+    TextView myTextView;
+
     @Nullable
     @Override
     public View onCreateView(@NonNull LayoutInflater inflater, @Nullable ViewGroup container, @Nullable Bundle savedInstanceState) {
         // show the current/popular shows
 //        return inflater.inflate(R.layout.fragment_home, container, false);
         View view = inflater.inflate(R.layout.fragment_home, container, false);
+        Logger.addLogAdapter(new AndroidLogAdapter());
+
+        OkHttpClient okHttpClient = new OkHttpClient.Builder().build();
+
+        ApolloClient apolloClient = ApolloClient.builder()
+                .serverUrl("https://graphql.anilist.co")
+                .okHttpClient(okHttpClient)
+                .build();
         Button button = (Button)view.findViewById(R.id.btn_press);
         button.setOnClickListener(new View.OnClickListener() {
             @Override
             public void onClick(View v) {
                 Log.d("123test", "plz");
+                apolloClient.query(
+                        AnimeDataQuery.builder()
+                                .id(1)
+                                .build()
+                ).enqueue(new ApolloCall.Callback<AnimeDataQuery.Data>() {
+                    @Override
+                    public void onResponse(@NotNull Response<AnimeDataQuery.Data> response) {
+                        String data = "";
+                        myTextView = (TextView)getView().findViewById(R.id.text_box);
+                       data = response.getData().Media().title().romaji() +'\n' + response.getData().Media().title().native_();
+                        myTextView.setText(data);
+                        Logger.d(response.getData().toString());
+                    }
+                    @Override
+                    public void onFailure(@NotNull ApolloException e) {
+                        Logger.d(e.getLocalizedMessage(), "error");
+                    }
+                });
             }
         });
         return view;
